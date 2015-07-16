5d95a6df9c9cd5eddc5e3462f58d226cb7999bd1
Gabriel <zdsiyan@gmail.com>
2015-07-16 10:21:19 +0800

    第一次更新
    第一次更新

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//设置错误级别
+error_reporting(0);
+/** ///调试、找错时请去掉///前空格
+ini_set('display_errors',true);
+error_reporting(E_ALL); 
+set_time_limit(0);
+define('DEBUG',	true);
+// */
+
+//安装检查开始：如果您已安装过ThinkSNS，可以删除本段代码
+if(is_dir('install/') && !file_exists('install/install.lock')){
+	header('location:' . 'install/install.php');
+}
+//安装检查结束
+
+// //禁止前台访问后台，使用后台入口index.php
+// if($_REQUEST['app']=='admin'){
+// 	exit;
+// }
+
+//网站根路径设置
+define('SITE_PATH', dirname(__FILE__));
+
+//载入核心文件
+require(SITE_PATH.'/core/core.php');
+
+//实例化一个网站应用实例
+$App = new App();
+$App->run();
